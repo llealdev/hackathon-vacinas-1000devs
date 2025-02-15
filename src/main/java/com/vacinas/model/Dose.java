1464bd41bf74b4217f79ac64dceec1548bbbaa64
Caio Cesar <caiocesarassissouza64@gmail.com>
2025-02-15 16:51:29 -0300

    Adiciona models (POJOs)

--- src/main/java/com/vacinas/model/Dose.java
+++ src/main/java/com/vacinas/model/Dose.java
@@ -1,5 +1,58 @@
 package com.vacinas.model;
 
 public class Dose {
+    private int id;
+    private int idVacina;
+    private String dose;
+    private int idadeRecomendadaAplicacao;
     
+    public Dose(){
+        
+    }
+
+    public Dose(int id, int idVacina, String dose, int idadeRecomendadaAplicacao) {
+        setId(id);
+        setId_vacina(idVacina);
+        setDose(dose);
+        setIdade_recomendada_aplicacao(idadeRecomendadaAplicacao);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_vacina() {
+        return idVacina;
+    }
+
+    public void setId_vacina(int idVacina) {
+        this.idVacina = idVacina;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
+
+    public int getIdade_recomendada_aplicacao() {
+        return idadeRecomendadaAplicacao;
+    }
+
+    public void setIdade_recomendada_aplicacao(int idadeRecomendadaAplicacao) {
+        this.idadeRecomendadaAplicacao = idadeRecomendadaAplicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "ID:" + this.id + ", idVacina:" + this.idVacina + ", dose:" + this.dose + ", idadeRecomendadaAplicacao:"
+                + this.idadeRecomendadaAplicacao;
+    }
+
 }
--- src/main/java/com/vacinas/model/Imunizacoes.java
+++ src/main/java/com/vacinas/model/Imunizacoes.java
@@ -1,5 +1,103 @@
 package com.vacinas.model;
 
+import java.sql.Date;
+
 public class Imunizacoes {
     
+    private int id;
+    private int idPaciente;
+    private int idDose;
+    private Date dataAplicacao;
+    private String fabricante;
+    private String lote;
+    private String localApicacao;
+    private String profissionalAplicador;
+
+    public Imunizacoes(){
+        
+    }
+
+    public Imunizacoes(int id, int idPaciente, int idDose, Date dataAplicacao, String fabricante, String lote,
+        String localApicacao, String profissionalAplicador) {
+        setId(id);
+        setId_paciente(idPaciente);
+        setId_dose(idDose);
+        setDataAplicacao(dataAplicacao);
+        setFabricante(fabricante);
+        setLote(lote);
+        setLocalApicacao(localApicacao);
+        setProfissionalAplicador(profissionalAplicador);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_paciente() {
+        return idPaciente;
+    }
+
+    public void setId_paciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public int getId_dose() {
+        return idDose;
+    }
+
+    public void setId_dose(int idDose) {
+        this.idDose = idDose;
+    }
+
+    public Date getDataAplicacao() {
+        return dataAplicacao;
+    }
+
+    public void setDataAplicacao(Date dataAplicacao) {
+        this.dataAplicacao = dataAplicacao;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+
+    public String getLocalApicacao() {
+        return localApicacao;
+    }
+
+    public void setLocalApicacao(String localApicacao) {
+        this.localApicacao = localApicacao;
+    }
+
+    public String getProfissionalAplicador() {
+        return profissionalAplicador;
+    }
+
+    public void setProfissionalAplicador(String profissionalAplicador) {
+        this.profissionalAplicador = profissionalAplicador;
+    }
+
+    @Override
+    public String toString() {
+        return "ID:" + this.id + ", Id_paciente:" + this.idPaciente + ", Id_dose:" + this.idDose + ", DataAplicacao:"
+                + this.dataAplicacao + ", Fabricante:" + this.fabricante + ", Lote:" + this.lote + ", LocalApicacao:" + this.localApicacao
+                + ", ProfissionalAplicador:" + this.profissionalAplicador;
+    }
+
 }
